7536f01b13975219174096add7b09a6d222f7f13
bic-harness <59663022+bic-harness@users.noreply.github.com>
2022-06-22 16:05:51 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,9 +4,11 @@
 curl -O -L  https://releases.hashicorp.com/terraform/1.2.1/terraform_1.2.1_linux_amd64.zip
 unzip terraform_1.2.1_linux_amd64.zip
 mv ./terraform /usr/bin/
+
 ASDF_PATH=$HOME/.asdf
 TERRAFORM_VERSIONS="1.2.0 1.2.1 1.2.2 1.2.3"
-git clone https://github.com/asdf-vm/asdf.git $ASDF_PATH --branch v0.8.0
+
+git clone https://github.com/asdf-vm/asdf.git $ASDF_PATH --branch v0.10.0
 echo '. $ASDF_PATH/asdf.sh' >> ~/.bashrc
 echo PATH=\$PATH:\$HOME/.asdf/shims:\$HOME/.asdf/bin >> /root/.bashrc
 
